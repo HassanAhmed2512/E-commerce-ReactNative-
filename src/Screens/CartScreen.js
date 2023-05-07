b98a11b3b4cc8c489ec39ddcd9cdd41c04c03a9c
Ahmed-Maher <Ahmed-Maher-166@users.noreply.github.com>
2023-05-07 18:02:18 +0300

    ahmed-cart

--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -1,12 +1,87 @@
-import { Text , View } from 'native-base';
+import {  Box ,Center ,Text,ScrollView , Button,HStack} from 'native-base';
+import CartItem from './CartItems';
+import { StatusBar } from 'expo-status-bar';
+import Cart from './CartEmpty';
 import { React } from 'react';
+import { products ,Colors} from "../data/data";
+import FontAwesome from "@expo/vector-icons";
+import Buttone from "../Components/Buttone" ;
+
 
  function CartScreen() {
   return (
-    <View >
-      <Text> CartScreen </Text>
-    </View>
+<Box flex = {1} safeAreaTop >
+<Center w= "full" py={5} bg={Colors.subGreen}>
+   <Text  color ={Colors.black} fontSize= {20} bold> CartScreen </Text>
+   </Center>
+    {/* /*if cart empty*/  }
+
+   {/* <Cart/>  */}
+ <ScrollView showsVerticalScrollIndicator ={false}> 
+  <CartItem/>  
+ 
+
+<Center mt={5}>
+
+<HStack
+
+rounded={50}
+
+
+
+justifyContent ="space-between" bg={Colors.white}
+
+shadow={2}
+
+W="90%"
+
+
+pl={5}
+
+h={45}
+
+alignItems="center"
+>
+<Text pl={5}>Total</Text>
+
+<Button px={10} h={45} rounded={50} bg={Colors.main}
+_text={{
+
+  color: Colors.white, fontWeight: "semibold",
+  
+  }}
+  
+  _pressed={{
+  
+  bg: Colors.main,
+  
+  }}
+>
+
+
+$600
+
+</Button>
+
+</HStack>
+
+</Center> 
+ {/* Checkout */}
+
+<Center px={5}>
+
+<Buttone bg={Colors.black} color={Colors.white} mt={100}>
+</Buttone>
+
+</Center> 
+  
+ </ScrollView>  
+
+
+    </Box>
   );
 }
+
+;
 
 export default CartScreen ;
